531e06b40b8be60a23c28d8826a69116ec8db301
ender251121 <145127096+ender251121@users.noreply.github.com>
2023-09-15 15:46:56 +0200

    Add files via upload

---
+++ c004/gitc004/ex00/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enalvare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 15:38:19 by enalvare          #+#    #+#             */
+/*   Updated: 2023/08/31 21:51:15 by enalvare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/*int	main(void)
+{
+	int	size;
+	char	s1[] = "hola";
+
+	size = ft_strlen(s1);
+	printf("%d", size);
+	return (0);
+}*/
---
+++ c004/gitc004/ex01/ft_putstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enalvare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 16:29:58 by enalvare          #+#    #+#             */
+/*   Updated: 2023/08/31 21:50:27 by enalvare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	char	s1[] = "piscina distrito 42 agosto";
+
+	ft_putstr(s1);
+	return (0);
+}*/
